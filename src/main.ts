f3ad2c6c0f3a7b070284802c6feac6fc7578ae2c
Nathan Nunes <nathanpedronunes@hotmail.com>
2024-03-19 11:13:24 -0300

    feat: enable cors

--- src/main.ts
+++ src/main.ts
@@ -5,6 +5,7 @@ import { Transport } from '@nestjs/microservices';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.enableCors();
   app.useGlobalPipes(new ValidationPipe({
     whitelist: true,
     forbidNonWhitelisted: true
